c7738d9bb56b5e91466f09d29dadde920f999444
fabar <fabarj4@gmail.com>
2023-09-14 08:07:23 +0700

    masih ngerjain

--- imigrasi_cilegon_site/settings.py
+++ imigrasi_cilegon_site/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kanimcilegonnew',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
         # "OPTIONS": {
