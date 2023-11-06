56323dc0809efb2ffca2a973266ff15aa67a059a
Reky Rolen Kencana <reky.rolen@gmail.com>
2023-11-06 09:50:21 +0700

    Add data upload max memory size

--- imigrasi_cilegon_site/settings.py
+++ imigrasi_cilegon_site/settings.py
@@ -151,3 +151,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 210239488
